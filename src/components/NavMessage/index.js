d72037376d729a0c15d8b74ed562ea3b1e605162
FedericoAlmenara <fnalmenaradumur96@gmail.com>
2019-01-24 16:25:02 -0500

    getting an error that i cant fix

---
+++ src/components/NavMessage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import "./style.css";
+
+function NavMessage(){
+    return(
+      <div>
+        Click to start
+      </div>
+    )
+}
+
+export default NavMessage;
